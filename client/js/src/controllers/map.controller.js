47e14fa38eaa0f5c505ccf5ec6c6ec4094ada70a
intheon <allobon@gmail.com>
2016-05-09 21:33:42 +0100

    js confusion

--- client/js/src/controllers/map.controller.js
+++ client/js/src/controllers/map.controller.js
@@ -2,20 +2,31 @@
 
 function MapCtrl($scope, helpersSrv){
 
-	$scope.isEditing = false;
+	// Bootstrap the mofo
+	configureLeaflet($scope, helpersSrv);
 
-	let map = L.map('map-core', { zoomControl: false }).setView([51.505, -0.09], 13);
+
+	// A button the user has to click to switch to 'add skatepark' modes
+	$scope.isEditing 			= false;
+	$scope.isMarkerInProg 		= false;
+	$scope.lastMarker 			= null;
 
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/intheon/cinz0kw8i0006bgnmykeq58x6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW50aGVvbiIsImEiOiJjaW5lZ3RkaDUwMDc2d2FseHhldHl0Y3dyIn0.L1RWCbggwqkNegUc1ZIwJw').addTo(map);
-	
-	L.Icon.Default.imagePath = '../../img/leaflet/';
+	// Set a listener on the map instance
+	$scope.map.on("click", (event) => {
 
-	new L.Control.Zoom({ position: 'bottomleft' }).addTo(map);
+		// Dont do anything if edit mode is off
+		if (!$scope.isEditing)
+		{
+			return;
+		}
+		else
+		{
+			createTempMarker($scope, event.latlng);
+		}
 
-	L.easyButton( '<div class="waves-effect white lighten-4 btn-flat toggleControl">Add a park</div>', function(){
-		toggleEditButton($scope, helpersSrv);
-	}).addTo(map);
+	})
+
 
 }
 						
@@ -31,6 +42,46 @@ function toggleEditButton($scope, helpersSrv)
 	{
 		$scope.isEditing = false;
 		helpersSrv.toggleEditOff();
+	}
+}
+
+function configureLeaflet($scope, helpersSrv)
+{
+	$scope.map = L.map('map-core', { zoomControl: false }).setView([51.505, -0.09], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/intheon/cinz0kw8i0006bgnmykeq58x6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW50aGVvbiIsImEiOiJjaW5lZ3RkaDUwMDc2d2FseHhldHl0Y3dyIn0.L1RWCbggwqkNegUc1ZIwJw').addTo($scope.map);
+	
+	L.Icon.Default.imagePath = '../../img/leaflet/';
+
+	new L.Control.Zoom({ position: 'bottomleft' }).addTo($scope.map);
+
+	L.easyButton( '<div class="waves-effect white lighten-4 btn-flat toggleControl">Add a park</div>', function(){
+		toggleEditButton($scope, helpersSrv);
+	}).addTo($scope.map);
+
+}
+
+function createTempMarker($scope, position)
+{
+	if (!$scope.isMarkerInProg)
+	{
+		console.log("if: isMarkerInProg:",$scope.isMarkerInProg);
+		console.log("if: lastMarker:",$scope.lastMarker);
+
+		$scope.lastMarker = L.marker([position.lat, position.lng]).addTo($scope.map);
+		$scope.isMarkerInProg = true;
+	}
+	else 
+	{
+		if ($scope.isMarkerInProg)
+		{
+			console.log("else: isMarkerInProg:",$scope.isMarkerInProg);
+			console.log("else: lastMarker:",$scope.lastMarker);
+			$scope.lastMarker.remove();
+			$scope.lastMarker = L.marker([position.lat, position.lng]).addTo($scope.map);
+			$scope.isMarkerInProg = false;
+		}
+
 	}
 }
 
