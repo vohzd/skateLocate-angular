b2600a10a2116eca57f2c6d844374f542a567f86
Ben S <allobon@gmail.com>
2016-05-10 15:44:54 +0100

    init

--- client/js/src/controllers/map.controller.js
+++ client/js/src/controllers/map.controller.js
@@ -2,10 +2,11 @@
 
 function MapCtrl($scope, helpersSrv){
 
-	console.log("running");
-	/*
 	// Bootstrap the mofo
 	configureLeaflet($scope, helpersSrv);
+
+	/*
+
 
 
 	// A button the user has to click to switch to 'add skatepark' modes
@@ -50,6 +51,13 @@ function toggleEditButton($scope, helpersSrv)
 
 function configureLeaflet($scope, helpersSrv)
 {
+	$scope.init = {
+		lat: 51.505,
+		lng: -0.09,
+		zoom: 10
+	}
+
+	/*
 	$scope.map = L.map('map-core', { zoomControl: false }).setView([51.505, -0.09], 13);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/intheon/cinz0kw8i0006bgnmykeq58x6/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaW50aGVvbiIsImEiOiJjaW5lZ3RkaDUwMDc2d2FseHhldHl0Y3dyIn0.L1RWCbggwqkNegUc1ZIwJw').addTo($scope.map);
@@ -61,6 +69,7 @@ function configureLeaflet($scope, helpersSrv)
 	L.easyButton( '<div class="waves-effect white lighten-4 btn-flat toggleControl">Add a park</div>', function(){
 		toggleEditButton($scope, helpersSrv);
 	}).addTo($scope.map);
+	*/
 
 }
 
